f84679b4a4a8bd58e05de40eb7e6c7a65ff53646
Bohdan Rudenko <brudenko2804@gmail.com>
2024-07-02 18:49:34 +0300

    Complete the task

--- src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
+++ src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
@@ -1,13 +1,22 @@
 package com.epam.mjc.collections.map;
 
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class FunctionValueFinder {
     public boolean isFunctionValuePresent(List<Integer> sourceList, int requiredValue) {
-        Map<Integer, Integer> functionMap = calculateFunctionMap(sourceList);
+        Set<Integer> functionMap = calculateFunctionMap(sourceList);
+        return functionMap.contains(requiredValue);
     }
 
-    private Map<Integer, Integer> calculateFunctionMap(List<Integer> sourceList) {
+    private Set<Integer> calculateFunctionMap(List<Integer> sourceList) {
+        return sourceList.stream()
+                .map(this::function)
+                .collect(Collectors.toSet());
+    }
+
+    private int function(int x) {
+        return 5 * x + 2;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,20 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        if (sourceMap.isEmpty()) {
+            return new HashMap<>();
+        }
+
+        HashMap<String, Integer> swappedMap = new HashMap<>(sourceMap.size());
+
+        for (Map.Entry<Integer, String> entry : sourceMap.entrySet()) {
+            swappedMap.computeIfAbsent(entry.getValue(), k -> entry.getKey());
+        }
+
+        return swappedMap;
     }
 }
